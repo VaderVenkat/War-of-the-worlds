2e37bc29998afc71efce6f6ffac929562893e972
VaderVenkat <165001972+VaderVenkat@users.noreply.github.com>
2024-12-03 12:32:59 +0530

    Update PlayerBehavior.cs
    yesss we change

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -9,7 +9,7 @@ public class PlayerBehavior : MonoBehaviour
     private float _vInput;
     private float _hInput;
     private Rigidbody _rb;
-
+private float summa = 30f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
