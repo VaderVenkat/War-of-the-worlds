5bd6abadc954cd6c0e49e08165cbed261b4c1cc6
VaderVenkat <165001972+VaderVenkat@users.noreply.github.com>
2024-12-03 15:34:20 +0530

    Changes done sucess fully

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -9,11 +9,17 @@ public class PlayerBehavior : MonoBehaviour
     private float _vInput;
     private float _hInput;
     private Rigidbody _rb;
+    public float JumpVelocity = 5f;
+    private bool _isJumping;
+    public float DistanceToGround = 0.1f;
+    public LayerMask GroundLayer;
+    private CapsuleCollider _col;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
+        _col = GetComponent<CapsuleCollider>();
     }
 
     // Update is called once per frame
@@ -21,8 +27,10 @@ public class PlayerBehavior : MonoBehaviour
     {
         _vInput = Input.GetAxis("Vertical") * MoveSpeed;
         _hInput = Input.GetAxis("Horizontal") * RotateSpeed;
-       /* this.transform.Translate(Vector3.forward * _vInput * Time.deltaTime);
-        this.transform.Rotate(Vector3.up * _hInput * Time.deltaTime);*/
+        /* this.transform.Translate(Vector3.forward * _vInput * Time.deltaTime);
+         this.transform.Rotate(Vector3.up * _hInput * Time.deltaTime);*/
+        _isJumping |= Input.GetKeyDown(KeyCode.J);
+
     }
 
     private void FixedUpdate()
@@ -31,5 +39,22 @@ public class PlayerBehavior : MonoBehaviour
         Quaternion angleRot = Quaternion.Euler(rotation * Time.fixedDeltaTime);
         _rb.MovePosition(this.transform.position + this.transform.forward * _vInput * Time.fixedDeltaTime);
         _rb.MoveRotation(_rb.rotation * angleRot);
+
+       
+
+        if(IsGrounded() && _isJumping)
+        {
+            _rb.AddForce(Vector3.up * JumpVelocity, ForceMode.Impulse);
+        }
+        _isJumping = false;
     }
+
+    private bool IsGrounded()
+    {
+        Vector3 capsuleBottom = new Vector3(_col.bounds.center.x, _col.bounds.min.y, _col.bounds.center.z);
+        bool grounded = Physics.CheckCapsule(_col.bounds.center, capsuleBottom, DistanceToGround, GroundLayer, QueryTriggerInteraction.Ignore);
+
+        return grounded;
+    }
+
 }
