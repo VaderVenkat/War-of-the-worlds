514dbebbaa7df9dd6b30b9536e05ddc0239bee39
VaderVenkat <165001972+VaderVenkat@users.noreply.github.com>
2024-12-03 14:33:01 +0530

    Scripted add to github

---
+++ Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "Player")
+        {
+            Debug.Log("Player detected - attack!");
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.name == "Player")
+        {
+            Debug.Log("Player out of range ,resume patrol");
+        }
+    }
+
+}
