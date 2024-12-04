e393f8f1727f12c73008b6ee7455c04622019e52
VaderVenkat <165001972+VaderVenkat@users.noreply.github.com>
2024-12-05 00:55:13 +0530

    Update GameBehavior.cs
    changes done again

--- Assets/Scripts/GameBehavior.cs
+++ Assets/Scripts/GameBehavior.cs
@@ -4,3 +4,22 @@ public class GameBehavior : MonoBehaviour
  private int _playerHP = 10;
  // 1
  public int Items
+{
+ // 2
+ get { return _itemsCollected; }
+ // 3
+ set { 
+ _itemsCollected = value; 
+ Debug.LogFormat("Items: {0}", _itemsCollected);
+ }
+ }
+ // 4
+ public int HP 
+ {
+ get { return _playerHP; }
+ set { 
+ _playerHP = value; 
+ Debug.LogFormat("Lives: {0}", _playerHP);
+ }
+ }
+}
