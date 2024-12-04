7572d807ba3b485c33f73f44c2a1aa02653be7d5
VaderVenkat <165001972+VaderVenkat@users.noreply.github.com>
2024-12-04 15:10:51 +0530

    Update BulletBehaviour.cs
    change

--- Assets/Scripts/BulletBehaviour.cs
+++ Assets/Scripts/BulletBehaviour.cs
@@ -3,12 +3,12 @@ using UnityEngine;
 public class BulletBehaviour : MonoBehaviour
 {
     public float OnscreenDelay = 3f;
-public float venkat = 2001f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Destroy(this.gameObject, OnscreenDelay);
-Debug.Log(venkat);
+
     }
 
     // Update is called once per frame
