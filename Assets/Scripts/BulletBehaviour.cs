5b0e002dc36fb2b6083d90892f372cb5c1246cf9
VaderVenkat <165001972+VaderVenkat@users.noreply.github.com>
2024-12-04 15:08:43 +0530

    Update BulletBehaviour.cs
    gsvsh

--- Assets/Scripts/BulletBehaviour.cs
+++ Assets/Scripts/BulletBehaviour.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class BulletBehaviour : MonoBehaviour
 {
     public float OnscreenDelay = 3f;
-public flaot venkat = 2001f;
+public float venkat = 2001f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
