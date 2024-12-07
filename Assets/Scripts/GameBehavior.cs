aede3b0f54fa37e0178b860fbcad3c74c606855d
VaderVenkat <165001972+VaderVenkat@users.noreply.github.com>
2024-12-07 23:37:33 +0530

    Changes commited successfully!!!

--- Assets/Scripts/GameBehavior.cs
+++ Assets/Scripts/GameBehavior.cs
@@ -1,27 +1,54 @@
 using System;
 using UnityEngine;
+using TMPro; 
 
 public class GameBehavior : MonoBehaviour
 {
     private int _itemsCollected = 0;
     private int _playerHP = 10;
 
+    public int MaxItems = 4;
+
+    public TMP_Text HealthText;
+    public TMP_Text ItemText;
+    public TMP_Text ProgressText;
+
+    private void Start()
+    {
+        ItemText.text += _itemsCollected;
+        HealthText.text += _playerHP;
+    }
+
+    private int _itemscollected = 0;
     public int Items
     {
         get { return _itemsCollected; }
         set
         {
             _itemsCollected = value;
-            Debug.LogFormat("Items: {0}", _itemsCollected);
+            ItemText.text = " Items Collected : " + Items;
+
+            if (_itemsCollected >= MaxItems)
+            {
+                ProgressText.text = "You've found all the items !";
+            }
+            else
+            {
+
+                ProgressText.text = "Items found , only " + (MaxItems - _itemscollected) +"more !";
+            }
         }
     }
 
+    
     public int HP
     {
         get { return _playerHP; }
         set
         {
             _playerHP = value;
+
+            HealthText.text = "Player Health: " + HP;
             Debug.LogFormat("Lives: {0}", _playerHP);
         }
     }
